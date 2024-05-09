2ea73d19bc51c49f889f114d2f99fa3c491d384a
Md Mustakin Ebne Mazid <mustakinshvn@gmail.com>
2024-05-09 16:43:45 +0600

    Create tasks

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const app= express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-//routes
-app.get('/',(req,res)=>{
-    res.sendend('Hellow')
-})
+const app = express();
 
-app.get('/blog',(req,res)=>{
-    res.send("hello motherfucker Rumi")
-})
+//Memory
+let tasks=[];
+
+app.use(bodyParser.json());
+
+//Create task
+app.post('/tasks', (req,res)=>{
+    const {title, description, status}= req.body;
+    if (!title || !status) {
+        return res.status(400).json({ error: 'Title and status are required.' });
+      }
+
+      const newTask = {
+        id: tasks.length + 1,
+        title,
+        description,
+        status
+      };
+      
+      tasks.push(newTask);
+      res.status(201).json(newTask);
+
+});
+
+
+
 
 app.listen(3000,()=>{
     console.log('Node API is running on port 3000')
-})
+});
